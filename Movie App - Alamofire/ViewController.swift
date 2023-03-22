641a12363d1244d6bc89fbbe3142bad93c338876
Semanur Eserler <78433748+Senures@users.noreply.github.com>
2023-03-22 09:38:09 +0300

    Initial Commit

---
+++ Movie App - Alamofire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie App - Alamofire
+//
+//  Created by SEMANUR ESERLER on 22.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
